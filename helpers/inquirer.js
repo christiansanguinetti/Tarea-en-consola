d7c5e4eee871a36ed0ddf38b666fdf26bc97b1b6
christiansanguinetti <110428442+christiansanguinetti@users.noreply.github.com>
2023-03-01 10:30:54 -0300

    Add files via upload

---
+++ helpers/inquirer.js
@@ -0,0 +1,68 @@
+const inquirer = require('inquirer');
+require ('colors');
+
+const menuOpts = [{
+
+    type: 'list',
+    name: 'opcion',
+    message:'Que desea hacer?',
+    choices: [
+    {
+        value: "1",
+        name:"1. Crear Tarea"
+    },
+    {
+        value: "2",
+        name:"2. Listar Tarea"
+    },
+    {
+        value: "3",
+        name:"3. Listar Tarea Completada"
+    },
+    {
+        value: "4",
+        name:"4. Listar Tareas Pendientes"
+    },
+    {
+        value: "5",
+        name:"5. Completar Tarea(s) "
+    },
+    {
+        value: "6",
+        name:"6. Borrar tarea"
+    },
+    {
+        value: "0",
+        name:"0. Salir"
+    }
+]
+}]
+
+
+const inquirerMenu = async () => {
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción'.green);
+    console.log('==========================\n'.green);
+ 
+   const {opcion} = await inquirer.prompt( menuOpts );
+   return {opcion};
+}
+   const pausa = async() => {
+     const question = [
+      {
+        type:"input",
+        name:"enter",
+       message:`Presione ${'enter'.green} para continuar`
+      } 
+     ]
+   
+   
+     await inquirer.prompt(question) 
+   }
+
+
+module.exports = {
+    inquirerMenu,
+    pausa
+}
