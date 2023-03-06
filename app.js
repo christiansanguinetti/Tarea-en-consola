f9fa157b91e08c9f58fd27d99c2dd3eb3849be8d
christiansanguinetti <110428442+christiansanguinetti@users.noreply.github.com>
2023-03-06 20:04:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu,
+    pausa,
+    leerimput } = require('./helpers/inquirer');
 require('colors');
-const {Tarea} = require('./models/Tarea.js');
-const {Tareas} = require('./models/Tareas.js');
+const Tarea = require('./models/Tarea.js');
+const { Tareas } = require('./models/Tareas.js');
 
 console.clear();
 
 const main = async () => {
-
+ 
     let opt = '';
+    const tareas = new Tareas();
 
     do {
-        //opt = await inquirerMenu();
-       // console.log( opt );
-        
-     const tarea = new Tarea (" Comprar Comida ");
-     console.log(tarea);
 
-
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                //crear opcion
+                const desc = await leerimput('descripcion:');
+                console.log(desc)
+                break;
+            case '2':''
+            console.log( tareas._listado);
+            break;
+        }
         await pausa();
 
     } while (opt !== '0');
