74a7b786fe56dd3dcbcb6f599be6477273f11e17
christiansanguinetti <110428442+christiansanguinetti@users.noreply.github.com>
2023-03-06 20:05:59 -0300

    Add files via upload

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,41 +1,41 @@
 const inquirer = require('inquirer');
-require ('colors');
+require('colors');
 
 const menuOpts = [{
 
     type: 'list',
     name: 'opcion',
-    message:'Que desea hacer?',
+    message: 'Que desea hacer?',
     choices: [
-    {
-        value: "1",
-        name:"1. Crear Tarea"
-    },
-    {
-        value: "2",
-        name:"2. Listar Tarea"
-    },
-    {
-        value: "3",
-        name:"3. Listar Tarea Completada"
-    },
-    {
-        value: "4",
-        name:"4. Listar Tareas Pendientes"
-    },
-    {
-        value: "5",
-        name:"5. Completar Tarea(s) "
-    },
-    {
-        value: "6",
-        name:"6. Borrar tarea"
-    },
-    {
-        value: "0",
-        name:"0. Salir"
-    }
-]
+        {
+            value: "1",
+            name: "1. Crear Tarea"
+        },
+        {
+            value: "2",
+            name: "2. Listar Tarea"
+        },
+        {
+            value: "3",
+            name: "3. Listar Tarea Completada"
+        },
+        {
+            value: "4",
+            name: "4. Listar Tareas Pendientes"
+        },
+        {
+            value: "5",
+            name: "5. Completar Tarea(s) "
+        },
+        {
+            value: "6",
+            name: "6. Borrar tarea"
+        },
+        {
+            value: "0",
+            name: "0. Salir"
+        }
+    ]
 }]
 
 
@@ -44,25 +44,53 @@ const inquirerMenu = async () => {
     console.log('=========================='.green);
     console.log('  Seleccione una opción'.green);
     console.log('==========================\n'.green);
- 
-   const {opcion} = await inquirer.prompt( menuOpts );
-   return {opcion};
+
+    const { opcion } = await inquirer.prompt(menuOpts);
+    return { opcion };
 }
-   const pausa = async() => {
-     const question = [
-      {
-        type:"input",
-        name:"enter",
-       message:`Presione ${'enter'.green} para continuar`
-      } 
-     ]
-   
-   
-     await inquirer.prompt(question) 
-   }
+const pausa = async () => {
+    const question = [
+        {
+            type:
+
+                "input",
+            name: "enter",
+            message: `Presione ${'enter'.green} para continuar`
+        }
+    ]
+
+
+    await inquirer.prompt(question)
+}
+
+const leerimput = async (menssage) => {
+    const question = [
+        {
+            type: "imput",
+            name: "desc",
+            menssage,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor Ingrese un valor';
+                }
+                return true
+            }
+        }
+
+
+
+    ];
+    const {desc} = await inquirer.prompt(question)
+    return desc
+
+
+
+}
+
 
 
 module.exports = {
     inquirerMenu,
-    pausa
+    pausa,
+    leerimput
 }
